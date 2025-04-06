f313ba2f2cc79df5dc968103111eccbebb56ff85
VITOCALCA85 <vitocalca85@gmail.com>
2025-04-06 10:54:03 -0300

    Add files via upload

---
+++ desafio 2.py
@@ -0,0 +1,9 @@
+# Definir los valores predefinidos
+largo = 4
+ancho = 6
+# Calcular el área del rectángulo
+área = largo * ancho
+# Mostrar el resultado
+print(f"El área de un rectángulo con un largo de {largo} unidades y un ancho de {ancho} unidades es:")
+print(f"Área = {area} unidades cuadradas.")
+Área = 24 unidades cuadradas.
---
+++ desafío 1.py
@@ -0,0 +1,10 @@
+ def sumar(a, b):
+     return a + b
+
+     resultado = sumar(4, 10)
+     El resultado de la suma es: 14
+     
+     
+     
+
+
