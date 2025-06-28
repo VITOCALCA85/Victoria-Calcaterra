8907533dc07deba7adf655ebe420e0dc19a7963c
VITOCALCA85 <vitocalca85@gmail.com>
2025-06-27 21:07:10 -0300

    Add files via upload

---
+++ contador.uy.py
@@ -0,0 +1,22 @@
+Una función recursiva cuenta_regresiva(n) que imprima los números desde n hasta 0, uno por línea.
+#contador.py
+   def es_par_o_impar(numero):
+Saber si el número es par o impar desde n hasta 0
+Pongo que 2 es un número par 
+if número 2 es par 
+return "par"
+else número 3 es impar 
+Pongo que 3 es número impar
+return "impar"
+Tengo que saber el resultado y voy a imrpimir
+print(f"{n} - {es_par_o_impar(n)}")
+
+
+
+    
+
+
+
+
+
+
